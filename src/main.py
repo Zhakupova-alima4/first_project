4a4077852dfa44fadf4ae8ee650e54fc6ef87f3b
Alima Churegeyeva <zhakupova-alima4@yandex.kz>
2026-03-20 23:53:59 +0500

    Структура проекта привидена в порядок

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    load_dotenv()  # Загружаем переменные из .env
+    author = os.getenv("AUTHOR")  # Читаем переменную AUTHOR
+    print(f"Автор проекта: {author}")
+
+if __name__ == "__main__":
+    print_author()
